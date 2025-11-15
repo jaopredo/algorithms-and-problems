411d4c7a358855007fb1ebd09b98b89bb2afc697
jaopredo <joao.profissional.45@gmail.com>
2025-11-14 23:23:30 -0300

    Correção DFS

--- src/graphs/path-exists/__main__.py
+++ src/graphs/path-exists/__main__.py
@@ -7,24 +7,40 @@ sys.path.insert(0, ROOT)
 from modules.graphs import GraphAdjacencyList
 
 
-def dfs(G: GraphAdjacencyList, v1: int, v2: int):
+def dfs(G: GraphAdjacencyList) -> list[int]:
+    """Recebe um grafo G e retorna a lista de visita do pre-order
+
+    Args:
+        G (GraphAdjacencyList): Grafo a ser analisado
+
+    Returns:
+        list[int]: Lista de pre-order
+    """
     n = len(G)
     visited = [0 for _ in range(n)]
 
+    preorder = [-1 for _ in range(n)]
+    visit = 1
+
     def reach_recursive(v):
-        v_neighbours = G.get_neighbours(v)
+        nonlocal visit
 
         # Se eu ja vizitei o nó atual
         if visited[v]:
             return
-        else:
-            visited[v] = 1
-            for node in v_neighbours:
-                reach_recursive(node)
+        
+        v_neighbours = G.get_neighbours(v)
 
-    reach_recursive(v1)
+        visited[v] = 1
+        preorder[v] = visit
+        visit += 1
+        for node in v_neighbours:
+            reach_recursive(node)
 
-    return bool(visited[v2])
+    for i in range(n):
+        reach_recursive(i)
+
+    return preorder
 
 
 G = GraphAdjacencyList()
@@ -36,4 +52,4 @@ edges = [(0,1),(1,2),(3,4)]
 for edge in edges:
     G.add_edge(edge[0], edge[1])
 
-print(dfs(G, 1, 4))
+print(dfs(G))
